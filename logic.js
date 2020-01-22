c20617ca2bfe3e5397d6f20d158898f14f6f460a
Spaghetti Jones <connor.breault@comcast.net>
2020-01-21 21:52:52 -0700

    firebase linked

---
+++ logic.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBqb9rA1e9tT3uCObNH25CzqPXGGulVbGM",
+    authDomain: "flubclub-rubhub.firebaseapp.com",
+    databaseURL: "https://flubclub-rubhub.firebaseio.com",
+    projectId: "flubclub-rubhub",
+    storageBucket: "flubclub-rubhub.appspot.com",
+    messagingSenderId: "563265812210",
+    appId: "1:563265812210:web:1434b64f9dc816096f026b",
+    measurementId: "G-GW6DJGRPD4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database()
